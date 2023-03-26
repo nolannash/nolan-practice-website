5ce52fe067bfe6a7d2aa0ad3ed20892c854a4521
nolan <n0lan.n4sh@gmail.com>
2023-03-26 14:54:10 -0700

    added 'myList' and changed some css

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This is a practice website";
 document.querySelector("body").appendChild(h2);
